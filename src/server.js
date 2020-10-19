fdbf32c394b48b94fa4daee9ca3db8d50cc22027
Arnold Gandarillas Castillo <arkgast@gmail.com>
2020-10-19 08:10:08 -0400

    feat: start server using tls

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('Hi!')
+const https = require('https')
+const fs = require('fs')
+const express = require('express')
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hi!' })
+})
+
+const httpsServer = https.createServer(options, app)
+
+const PORT = 3001
+httpsServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
